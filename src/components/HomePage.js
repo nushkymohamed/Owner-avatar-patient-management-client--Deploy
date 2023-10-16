b517a0186ef4243cac4f51b27b33ba2c6855faf0
nusky <it19167442@my.sliit.lk>
2023-10-16 09:52:28 +0530

    Link tag replaced with a tag

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -7,46 +7,45 @@ import AssessmentIcon from '@mui/icons-material/Assessment';
 function HomePage() {
   return (
     <div className=" min-h-screen md:flex md:flex-col flex-row  md:items-center md:justify-center md:px-[0%] px-[20%] ">
-      <div className='pt-[30px]'>
-      <h1 className="md:text-3xl font-bold mb-8 text-gray-700">
-        Patient Management System
-      </h1>
+      <div className="pt-[30px]">
+        <h1 className="md:text-3xl font-bold mb-8 text-gray-700">
+          Patient Management System
+        </h1>
       </div>
-     
+
       <div className="md:flex gap-[20px] pt-[20px]">
-        <Link to="/Listpatients">
+        <a href="/Listpatients">
           <div className="text-lg text-white h-[125px] w-[250px] text-center bg-yellow-400 border rounded-lg shadow-md hover:bg-yellow-300">
             <div className="pt-[25px]"> Manage Patients</div>
-            <div className='pt-[10px]'>
-            <Person2Icon fontSize='large'/>
+            <div className="pt-[10px]">
+              <Person2Icon fontSize="large" />
             </div>
-          
           </div>
-        </Link>
-        <Link to="/Listprescriptions">
+        </a>
+        <a href="/Listprescriptions">
           <div className="text-lg  text-white h-[125px] w-[250px] text-center bg-red-700 border rounded-lg shadow-md hover:bg-red-600">
             <div className="pt-[25px]"> Prescriptions</div>
-            <div className='pt-[10px]'>
-            <MedicationIcon fontSize='large'/>
+            <div className="pt-[10px]">
+              <MedicationIcon fontSize="large" />
             </div>
           </div>
-        </Link>
-        <Link to="/ListBillPayments">
+        </a>
+        <a href="/ListBillPayments">
           <div className="text-lg  text-white h-[125px] w-[250px] text-center bg-green-600 border rounded-lg shadow-md hover:bg-green-500">
             <div className="pt-[25px]"> Bill Payments </div>
-            <div className='pt-[10px]'>
-            <PaymentIcon fontSize='large'/>
+            <div className="pt-[10px]">
+              <PaymentIcon fontSize="large" />
             </div>
           </div>
-        </Link>
-        <Link to="/RevenueReport">
+        </a>
+        <a href="/RevenueReport">
           <div className="text-lg  text-white h-[125px] w-[250px] text-center bg-blue-400 border rounded-lg shadow-md hover:bg-blue-300">
             <div className="pt-[25px]"> Revenue Reports </div>
-            <div className='pt-[10px]'>
-            <AssessmentIcon fontSize='large'/>
+            <div className="pt-[10px]">
+              <AssessmentIcon fontSize="large" />
             </div>
           </div>
-        </Link>
+        </a>
       </div>
     </div>
   );
